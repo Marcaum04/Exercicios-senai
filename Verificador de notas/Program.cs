2853ddad730e49b24db85387b0e0b7fb1f42e9e4
Marcos <mvinicius.oliveira04@gmail.com>
2021-05-05 20:56:19 -0300

    C# verificador de notas

---
+++ Verificador de notas/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Verificador_de_notas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota;
+            bool notaValida;
+
+            Console.Write("Escreva uma nota de 1 a 10: ");
+            nota = double.Parse(Console.ReadLine());
+            notaValida = false;
+
+            while (notaValida == false)
+            {
+                if (nota <= 10 && nota >= 0)
+                {
+                    notaValida = true;
+                    Console.WriteLine("Obrigado sua nota foi armazenada.");
+                }
+                else
+                {
+
+                    Console.Write("Nota inválida escreva uma nota de 1 a 10: ");
+                    nota = double.Parse(Console.ReadLine());
+                }
+
+            }
+        }
+    }
+}
+
+
