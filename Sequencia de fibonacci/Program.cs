4f1fd8ac0f80045885672107807288548db4a108
Marcos <mvinicius.oliveira04@gmail.com>
2021-05-05 21:57:38 -0300

    C# fibonacci

---
+++ Sequencia de fibonacci/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Sequencia_de_fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        int a, b, c;
+
+	    a = -1;
+	    b = 1;
+	    c = 0;
+
+	    while(c < 500){
+		c = a + b;
+		a = b;
+		b = c;
+		Console.Write(c + " ");
+		} 
+        }
+    }
+}
