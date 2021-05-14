8223b292048824ea2a31a9e21d84e19f80c71fad
Marcos <mvinicius.oliveira04@gmail.com>
2021-05-14 10:18:42 -0300

    Catálogo de produtos com funções

--- Catálogo de produtos/Program.cs
+++ Catálogo de produtos/Program.cs
@@ -4,20 +4,21 @@ namespace Catálogo_de_produtos
 {
     class Program
     {
+        static int array = 10;
+        static string[] nome = new string[array];
+        static float[] preco = new float[array];
+        static bool[] desconto = new bool[array];
+        static int cont = 0;
         static void Main(string[] args)
         {
-            int array = 10;
-            string[] nome = new string[array];
-            float[] preco = new float[array];
-            int[] desconto = new int[array];
-            string promocao;
+            Menu();
+        }
 
+        static void Menu(){
             int menu;
-            int cont = 0;
-            int novoProduto;
-do
-{
-            Console.Write(@"
+            do
+            {
+                Console.Write(@"
 ______________________________
 ||Bem-vindo a vendinha do zé||
 ||__________________________||            
@@ -28,30 +29,48 @@ ______________________________
 ||         0 - Sair         ||
 ||__________________________||
 ||  Digite:  ");
-            menu = int.Parse(Console.ReadLine());
-            Console.Clear();
+                menu = int.Parse(Console.ReadLine());
+                Console.Clear();
 
-            switch (menu)
-            {
-                case 1:
-                    do
-                    {
+                switch (menu)
+                {
+                    case 1:
+                        CadastrarProduto();
+                        break;
+                    case 2:
+                        ListarProdutos();
+                        break;
+                    case 0:
+                        Console.WriteLine(@"Obrigado por catalogar na vendinha do zé
+            Volte sempre!");
+                        break;
+                }
 
-                        Console.Write(@"
+            } while (menu != 0);
+
+        }
+        static void CadastrarProduto()
+        {
+
+            int novoProduto;
+
+                        do
+                        {
+                            Console.Write(@"
 _________________________________            
 ||    Qual nome do produto?    || 
 ||_____________________________||
 ||  Digite: ");
-                        nome[cont] = Console.ReadLine();
+                            nome[cont] = Console.ReadLine();
 
-                        Console.Write(@"
+                            Console.Write(@"
 _________________________________            
 ||    Qual preço do produto?   || 
 ||_____________________________||
 ||  Digite: ");
-                        preco[cont] = float.Parse(Console.ReadLine());
+                            preco[cont] = float.Parse(Console.ReadLine());
 
-                        Console.Write(@"
+                            Console.Write(@"
 _________________________________            
 || O produto está em promoção? || 
 ||_____________________________||
@@ -59,10 +78,15 @@ _________________________________
 ||          0 - Não            ||
 ||_____________________________||
 ||  Digite: ");
-                        desconto[cont] = int.Parse(Console.ReadLine());
-                        cont++;
+                            string resposta = Console.ReadLine();
+                            if(resposta == "1"){
+                            desconto[cont] = true;
+                            }else{
+                            desconto[cont] = false;
+                            }
+                            cont++;
 
-                        Console.Write(@"
+                            Console.Write(@"
 _____________________________________            
 || Deseja cadastrar outro produto? || 
 ||_________________________________||
@@ -70,24 +94,27 @@ _____________________________________
 ||            0 - Não              ||
 ||_________________________________||
 ||  Digite: ");
-                        novoProduto = int.Parse(Console.ReadLine());
-                    } while (novoProduto == 1);
-                    break;
-                case 2:
-                    for (var i = 0; i < cont; i++)
-                    {
-                        if (desconto[i] == 1)
-                        {
-                            promocao = "Sim";
-                        }
-                        else
-                        {
-                            promocao = "Não";
-                        }
-                        if (nome[i] != null)
-                        {
+                            novoProduto = int.Parse(Console.ReadLine());
+                        } while (novoProduto == 1);
+        }
+        static void ListarProdutos(){
 
-                            Console.Write($@"
+            string promocao;
+
+                        for (var i = 0; i < cont; i++)
+                        {
+                            if (desconto[i] == true)
+                            {
+                                promocao = "Sim";
+                            }
+                            else
+                            {
+                                promocao = "Não";
+                            }
+                            if (nome[i] != null)
+                            {
+
+                                Console.Write($@"
 ______________________________            
 ||        Produto {i + 1}    
 ||__________________________||
@@ -95,16 +122,8 @@ ______________________________
 ||      Preço: {preco[i]:C2}    
 ||      Em Promoção: {promocao}         
 ||__________________________|| ");
+                            }
                         }
-                    }
-                    break;
-                case 0:
-                    Console.WriteLine(@"Obrigado por catalogar na vendinha do zé
-            Volte sempre!");
-                    break;
-            }
-
-} while (menu != 0);
         }
     }
 }
