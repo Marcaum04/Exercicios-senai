f709d4178ec0ceadcee1b096c66ff09e1c1948c8
Marcos <mvinicius.oliveira04@gmail.com>
2021-05-27 11:30:26 -0300

    POO cafeteria otimizada

--- POO Cafeteira - Entrega/Program.cs
+++ POO Cafeteira - Entrega/Program.cs
@@ -7,10 +7,40 @@ namespace POO_Cafeteira___Entrega
     {
         static void Main(string[] args)
         {
+            bool outroCafe = true;
+            bool cafe = true;
             Cafeteira cafeteira = new Cafeteira();
 
-            Console.WriteLine("Super CafeteiraTabajaras Plus++ inicializada");
-            Console.WriteLine(cafeteira.fazerCafe());
+            Console.WriteLine(@"
+Super CafeteiraTabajaras Plus++ inicializada");
+            do
+            {
+                cafeteira.fazerCafe();
+                do
+                {
+
+                    Console.WriteLine($@"
+Sim(S) / Não(N)
+Deseja faze outro café?");
+                    string Sn = Console.ReadLine().ToLower();
+                    if (Sn == "s")
+                    {
+                        outroCafe = false;
+                        cafe = true;
+                    }
+                    else if (Sn == "n")
+                    {
+                        outroCafe = true;
+                        cafe = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine(@"
+Comando inválido");
+                        cafe = false;
+                    }
+                } while (cafe == false);
+            } while (outroCafe == false);
         }
     }
 }
